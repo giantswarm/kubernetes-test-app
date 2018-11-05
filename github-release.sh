7e7cdf8616093e6de5d94d798bd929867a40844d
Tobias Richter <tr4711@gmail.com>
2018-11-05 17:24:18 +0100

    Create Tag first

--- github-release.sh
+++ github-release.sh
@@ -5,6 +5,26 @@ PERSONAL_ACCESS_TOKEN=$3
 SHORT_SHA=$(echo ${SHA} | head -c 7)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
+echo "Creating Tag"
+tag_output=$(curl \
+    --request POST \
+    --header "Authorization: token $PERSONAL_ACCESS_TOKEN" \
+    --header "Content-Type: application/json" \
+    --data "{
+        \"tag\": \"${SHA}\",
+        \"message\": \"automated tag for ${SHORT_SHA}\",
+        \"object\": \"${SHA}\",
+        \"type\": \"commit\",
+        \"tagger\": {
+            \"name\": \"taylorbot\",
+            \"email\": \"dev@giantswarm.io\",
+            \"date\": \"${DATE}\"
+        }
+    }" \
+    https://api.github.com/repos/giantswarm/${PROJECT}/git/tags
+)
+echo $tag_output | jq
+
 echo "Creating GitHub Release"
 release_output=$(curl \
     --request POST \
