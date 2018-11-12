df0413ec798f64be8b26de5865df028e9f6841c7
Tobias Richter <tr4711@gmail.com>
2018-11-12 17:09:05 +0100

    Add git credentials

--- app-release.sh
+++ app-release.sh
@@ -6,4 +6,9 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 tar=$(helm package --save=false helm/kubernetes-test-app-chart | tr "/" " " | awk '{print $NF}')
 mkdir -p /home/circleci/.helm/plugins
 helm plugin install https://github.com/technosophos/helm-github
+
+
+git config credential.helper 'cache --timeout=120'
+git config user.email "dev@giantswarm.io"
+git config user.name "Taylor Bot"
 helm github push $tar
