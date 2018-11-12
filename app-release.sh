4b05d3906ed6642121d3674381c31a248568f402
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:21:35 +0100

    add app-release script

---
+++ app-release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+helm package helm/kubernetes-test-app-chart
+tar=$(helm package helm/kubernetes-test-app-chart | awk '{print $NF}')
+helm plugin install https://github.com/technosophos/helm-github
+helm github push $tar
