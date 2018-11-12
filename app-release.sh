e2e066d01849f62f4372fb9657f72aedd145cac7
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:56:41 +0100

    Also make parent dir

--- app-release.sh
+++ app-release.sh
@@ -4,7 +4,7 @@ set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
-mkdir /home/circleci/.helm/plugins
+mkdir -p /home/circleci/.helm/plugins
 echo $HELM_HOME
 helm plugin install https://github.com/technosophos/helm-github
 helm github push $tar
