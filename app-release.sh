2bb63facf3a3345267120a7b992c040c0e70ac54
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:44:57 +0100

    Remove double helm package call

--- app-release.sh
+++ app-release.sh
@@ -3,7 +3,6 @@
 set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-helm package helm/kubernetes-test-app-chart
-tar=$(helm package helm/kubernetes-test-app-chart --save=false | awk '{print $NF}')
+tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
 helm plugin install https://github.com/technosophos/helm-github
 helm github push $tar
