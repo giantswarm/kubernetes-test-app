92132dc1fcff128e424e9ace982b24cb1e280725
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:38:03 +0100

    Dont save to local repository

--- app-release.sh
+++ app-release.sh
@@ -4,6 +4,6 @@ set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 helm package helm/kubernetes-test-app-chart
-tar=$(helm package helm/kubernetes-test-app-chart | awk '{print $NF}')
-#helm plugin install https://github.com/technosophos/helm-github
-#helm github push $tar
+tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
+helm plugin install https://github.com/technosophos/helm-github
+helm github push $tar
