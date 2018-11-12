b30b6a74077682042550d1f70987e70abc7681ca
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:54:36 +0100

    Make dir in absolutepath

--- app-release.sh
+++ app-release.sh
@@ -4,6 +4,7 @@ set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
-mkdir $HELM_HOME/plugins
+mkdir /home/circleci/.helm/plugins
+echo $HELM_HOME
 helm plugin install https://github.com/technosophos/helm-github
 helm github push $tar
