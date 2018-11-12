dc0e0e23ea72db54617792fbc6aa4c91e6995523
Tobias Richter <tr4711@gmail.com>
2018-11-12 16:48:26 +0100

    Make plugins dir

--- app-release.sh
+++ app-release.sh
@@ -4,5 +4,6 @@ set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
+mkdir $(HELM_HOME)/plugins
 helm plugin install https://github.com/technosophos/helm-github
 helm github push $tar
