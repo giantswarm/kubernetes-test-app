237feddeccdd3000d1bb0a55317131969d887398
Tobias Richter <tr4711@gmail.com>
2018-11-12 17:06:15 +0100

    Get only tar name

--- app-release.sh
+++ app-release.sh
@@ -3,7 +3,7 @@
 set -eu
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-tar=$(helm package --save=false helm/kubernetes-test-app-chart | awk '{print $NF}')
+tar=$(helm package --save=false helm/kubernetes-test-app-chart | tr "/" " " | awk '{print $NF}')
 mkdir -p /home/circleci/.helm/plugins
 helm plugin install https://github.com/technosophos/helm-github
 helm github push $tar
