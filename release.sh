d97df24a8d466492570f84a7c838930ee06551b5
Tobias Richter <tr4711@gmail.com>
2018-12-03 10:18:59 +0100

    First rewrite

--- release.sh
+++ release.sh
@@ -1,104 +1,105 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Script to release a managed app
+set -o errexit
+set -o nounset
+set -o pipefail
 
-PROJECT=kubernetes-test-app
-DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-SHA=$(git pull origin master > /dev/null 2>&1 & git rev-parse HEAD)
-VERSION=$(cat ./VERSION)
+readonly PROJECT=$1
+readonly SHA=$2
+readonly VERSION=$3
+readonly GITHUB_TOKEN=$4
 
-usage() {
-cat << EOF
+readonly HELM_URL=https://storage.googleapis.com/kubernetes-helm
+readonly HELM_TARBALL=helm-v2.11.0-linux-amd64.tar.gz
 
-Managed Apps Release Script
+main() {
+  if ! setup_helm_client; then
+    log_error "Helm client could not get installed"
+    return 1
+  fi
 
-Create a Git tag and GitHub release based on the SemVer specified in
-the VERSION file of the current project. The CI-Pipeline will push
-the chart to Helm repositories which are defined in the CATALOGS file,
-on detection of a the new SemVer Git Tag.
+  if ! id=(release_github "${PROJECT}" "${TAG}" "${GITHUB_TOKEN}"); then
+    log_error "GitHub Release could not get created"
+    return 1
+  fi
 
-Note: A valid Github Token with write access to the repo has to be placed
-      in '${HOME}/.github_token' or are set as ENV \$GITHUB_TOKEN.
+  if ! upload_assets "${PROJECT}" "${TAG}" "${GITHUB_TOKEN} ${id}"; then
+    log_error "Assets could not be uploaded to GitHub"
+    return 1
+  fi
 
-      https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
-EOF
 }
 
-release() {
-  echo "Releasing ${PROJECT}..."
-  echo "Creating Tag v${VERSION}"
-  tag_output=$(curl \
-      --request POST \
-      --header "Authorization: token ${GITHUB_TOKEN}" \
-      --header "Content-Type: application/json" \
-      --data "{
-          \"tag\": \"v${VERSION}\",
-          \"message\": \"Automated tag for ${VERSION}\",
-          \"object\": \"${SHA}\",
-          \"type\": \"commit\",
-          \"tagger\": {
-              \"name\": \"taylorbot\",
-              \"email\": \"dev@giantswarm.io\",
-              \"date\": \"${DATE}\"
-          }
-      }" \
-      https://api.github.com/repos/giantswarm/${PROJECT}/git/tags
-  )
-  echo ${tag_output} | jq
-  echo "Done. The managed app will now be pushed to a helm repo."
-  echo ""
+setup_helm_client() {
+    echo "Setting up Helm client..."
 
-  echo "Creating Github release v${VERSION}"
+    curl --user-agent curl-ci-sync -sSL -o "${HELM_TARBALL}" "${HELM_URL}/${HELM_TARBALL}"
+    tar xzfv "${HELM_TARBALL}"
+
+    PATH="$(pwd)/linux-amd64/:$PATH"
+
+    helm init --client-only
+}
+
+release_github() {
+  local project="${1?Specify project}"
+  local version="${2?Specify version}"
+  local token="${3?Specify Github Token}"
+
+
+  wget --no-check-certificate "https://github.com/giantswarm/${project}/tarball/v${version}" > /dev/null 2>&1
+  if [ $? -eq 0 ];then
+    log_error "Release already exists."
+    exit 1
+  fi
+
+  echo "Creating Github release v${version}"
   release_output=$(curl -s \
       -X POST \
-      -H "Authorization: token ${GITHUB_TOKEN}" \
+      -H "Authorization: token ${token}" \
       -H "Content-Type: application/json" \
       -d "{
-          \"tag_name\": \"v${VERSION}\",
-          \"name\": \"v${VERSION}\",
+          \"tag_name\": \"v${version}\",
+          \"name\": \"v${version}\",
           \"body\": \"### New features\\n\\n### Minor changes\\n\\n### Bugfixes\\n\\n\",
           \"draft\": false,
           \"prerelease\": false
       }" \
-      https://api.github.com/repos/giantswarm/${PROJECT}/releases
+      "https://api.github.com/repos/giantswarm/${project}/releases"
   )
   echo "The Github release is now published."
   echo "Please add release notes here:"
-  echo "https://github.com/giantswarm/${PROJECT}/releases/edit/v${VERSION}"
+  echo "https://github.com/giantswarm/${project}/releases/edit/v${version}"
 
-  # fetch the release id for the upload
-  RELEASE_ID=$(echo $release_output | jq '.id')
-
-  # Replace CI version with release VERSION
-  sed -i 's/version:.*/version: '${VERSION}'/' helm/${PROJECT}-chart/Chart.yaml
-
-  # Install helm and package chart TODO: Not use latest helm
-  curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-  CHART=$(helm package --save=false helm/${PROJECT}-chart | tr "/" " " | awk '{print $NF}')
-  echo "Upload chart ${CHART} to GitHub Release"
-  upload_output=$(curl -s \
-        -H "Authorization: token ${GITHUB_TOKEN}" \
-        -H "Content-Type: application/octet-stream" \
-        --data-binary @${CHART} \
-          https://uploads.github.com/repos/giantswarm/${PROJECT}/releases/${RELEASE_ID}/assets?name=${CHART}
-  )
-
-  # Cleanup
-  git checkout helm/${PROJECT}-chart/Chart.yaml
-  rm -f ${CHART}
+  # Return release id for the asset upload
+  release_id=$(echo $release_output | jq '.id')
+  echo ${release_id}
+  return 0
 }
 
-wget --no-check-certificate https://github.com/giantswarm/${PROJECT}/tarball/v${VERSION} > /dev/null 2>&1
-if [ $? -eq 0 ];then
-  echo "Release already exists. Did you increment the version in the VERSION file?"
-  exit 1
-elif ! [ -z "${GITHUB_TOKEN}" ]; then
-  release
-elif [ -e "${HOME}/.github-token" ]; then
-  GITHUB_TOKEN=$(cat ${HOME}/.github-token)
-  release
-else
-  echo "Error: No GitHub token found!"
-  usage
-  exit 1
-fi
+upload_assets(){
+  local project="${1?Specify project}"
+  local version="${2?Specify version}"
+  local token="${3?Specify Github Token}"
+  local release_id="${4?Specify Release Id}"
+
+  # Replace CI version with release VERSION
+  sed -i 's/version:.*/version: '${version}'/' "helm/${project}-chart/Chart.yaml"
+  chart=$(helm package --save=false "helm/${project}-chart" | tr "/" " " | awk '{print $NF}')
+
+  echo "Upload chart ${chart} to GitHub Release"
+  upload_output=$(curl -s \
+        -H "Authorization: token ${token}" \
+        -H "Content-Type: application/octet-stream" \
+        --data-binary @${chart} \
+          "https://uploads.github.com/repos/giantswarm/${project}/releases/${release_id}/assets?name=${chart}"
+  )
+  echo ${upload_output}
+  exit 0
+}
+
+log_error() {
+    printf '\e[31mERROR: %s\n\e[39m' "$1" >&2
+}
+
+main
